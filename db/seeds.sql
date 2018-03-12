c9679666c261e2058fdbbc8ac1d3ea9bac86bebe
Chetan Audipudy <chetan.audipudy@gmail.com>
2018-03-12 16:13:11 -0500

    basic folder structure and require

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Chicken' , false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Parm Chicken' , false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean' , false);
+
+
+select * from burgers;
